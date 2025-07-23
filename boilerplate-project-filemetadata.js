0f61d03fb5a2007f9e0a4b1955238cfb80238d29
inbuskuo <54679660+inbuskuo@users.noreply.github.com>
2025-07-23 10:50:06 +0800

    Add files via upload

---
+++ boilerplate-project-filemetadata.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer');
+const upload = multer({
+  limit:{
+    fileSize: 1000000 
+  }
+});
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const file = req.file;
+  const fileInfo = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  };
+  res.json(fileInfo);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
