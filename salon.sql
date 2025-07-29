55ccf1ad46541acc3e748375ea7d34bd504f329f
inbuskuo <54679660+inbuskuo@users.noreply.github.com>
2025-07-29 19:53:40 +0800

    Update salon.sql

--- salon.sql
+++ salon.sql
@@ -173,38 +173,35 @@ ALTER TABLE ONLY public.services ALTER COLUMN service_id SET DEFAULT nextval('pu
 -- Data for Name: appointments; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.appointments VALUES (1, 2, 4, '10:30');
-INSERT INTO public.appointments VALUES (2, 2, 5, '11am');
 
 
 --
 -- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.customers VALUES (2, '555-5555', 'Fabio');
 
 
 --
 -- Data for Name: services; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.services VALUES (4, 'cut');
-INSERT INTO public.services VALUES (5, 'color');
-INSERT INTO public.services VALUES (6, 'perm');
+INSERT INTO public.services VALUES (1, 'cut');
+INSERT INTO public.services VALUES (2, 'color');
+INSERT INTO public.services VALUES (3, 'perm');
 
 
 --
 -- Name: appointments_appointment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.appointments_appointment_id_seq', 2, true);
+SELECT pg_catalog.setval('public.appointments_appointment_id_seq', 8, true);
 
 
 --
 -- Name: customers_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.customers_customer_id_seq', 2, true);
+SELECT pg_catalog.setval('public.customers_customer_id_seq', 7, true);
 
 
 --
